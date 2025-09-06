61a47987d85d0b2afe020b2d43ac6b7d20e03001
Asik <asikhossain592445@gmail.com>
2025-09-06 23:42:16 +0600

    fixed button select bug and word or lesson bug

--- script/script.js
+++ script/script.js
@@ -6,18 +6,39 @@ const getLessons = () => {
     .then((resData) => getLessonsData(resData.data))
 }
 
+const activeClasRemove = () => {
+    const getAllButton = document.querySelectorAll('.lesson-button');
+    getAllButton.forEach((removeBtn) => {
+        removeBtn.classList.remove('active')
+    })
+}
 
 const levelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
     .then((response) => response.json())
-    .then((resData) => getWords(resData.data))
-}
-
+    .then((resData) => {
+        const getButton = document.getElementById(`lesson-button-${id}`);
+        activeClasRemove()
+        getButton.classList.add('active')    
+        getWords(resData.data)})
+    }
+                                                  
 const getWords = (wordArray) => {
     // Get the Words Container
     const getWordContainer = document.getElementById('words-container-id');
     getWordContainer.innerHTML = '';
+
+    if(wordArray.length == 0) {
+        getWordContainer.innerHTML = `
+        <div class="blank-lesson col-span-full flex flex-col items-center justify-between gap-4">
+            <img src="assets/alert-error.png" alt="Alert Error">
+            <p class="font-bangla text-xl font-medium text-gray-600">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h1 class="font-bangla text-3xl font-bold">নেক্সট Lesson এ যান</h1>
+        </div>
+        `;
+    }
+    
     wordArray.forEach(wordObj => {
         const word = wordObj.word;
         const pronounce = wordObj.pronunciation;
@@ -27,9 +48,9 @@ const getWords = (wordArray) => {
         
         createNewElement.innerHTML = `
                 <div class="word-card m-auto w-90 h-full bg-white rounded-md shadow-xl p-8 text-center flex flex-col gap-3">
-                    <h1 class="text-2xl font-bold">${word}</h1>
+                    <h1 class="text-2xl font-bold">${word ? word : 'শব্দ পাওয়া যায়নি'}</h1>
                     <p>Meaning / Pronounciation</p>
-                    <h1 class="text-2xl font-semibold text-gray-700 font-bangla">${meaning} / ${pronounce}</h1>
+                    <h1 class="text-2xl font-semibold text-gray-700 font-bangla">${meaning ? meaning : 'অর্থ পাওয়া যায়নি'} / ${pronounce ? pronounce : 'উচ্চারণ পাওয়া যায়নি'}</h1>
                     <div class="word-card-btn flex justify-between items-center mt-7">
                         <button class="w-12 h-12 rounded-sm bg-blue-100 text-gray-700 hover:bg-blue-300 cursor-pointer"><i class="fa-solid fa-circle-info"></i></button>
                         <button class="w-12 h-12 rounded-sm bg-blue-100 text-gray-700 hover:bg-blue-300 cursor-pointer"><i class="fa-solid fa-volume-high"></i></button>
@@ -57,7 +78,7 @@ const getLessonsData = (lessons) => {
 
         const createElement = document.createElement('div');
         createElement.innerHTML = `
-            <button onclick="levelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${getTheLessonId}</button>
+            <button id="lesson-button-${lesson.level_no}" onclick="levelWord(${lesson.level_no})" class="lesson-button btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${getTheLessonId}</button>
         `
 
         getParentElement.appendChild(createElement);
