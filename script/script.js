b15fb2e6d072c6b8721c1c93a4126ba994f66e3e
Asik <asikhossain592445@gmail.com>
2025-09-07 07:21:20 +0600

    partially design and implemented word details modal

--- script/script.js
+++ script/script.js
@@ -13,6 +13,8 @@ const activeClasRemove = () => {
     })
 }
 
+
+
 const levelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
@@ -23,7 +25,46 @@ const levelWord = (id) => {
         getButton.classList.add('active')    
         getWords(resData.data)})
     }
-                                                  
+    
+
+    // Word Details Function Program
+    const levelWordDetails = async (id) => {
+        const url = `https://openapi.programming-hero.com/api/word/${id}`
+        const getWordDetailsAPI = await fetch(url)
+        const response = await getWordDetailsAPI.json();
+        showLevelWordDetails(response.data)
+        
+    }
+
+    // Show Word Details
+    const showLevelWordDetails = async (wordDetails) => {
+        const getModalParent = document.getElementById('details-container-id');
+        getModalParent.innerHTML = `
+            <div class="p-2 flex flex-col gap-5">
+                    <h1 class="font-bold text-2xl">${wordDetails.word} (<i class="fa-solid fa-microphone-lines"></i> :${wordDetails.pronunciation})</h1>
+                    <div>
+                        <p class="font-semibold text-lg">Meaning</p>
+                        <p class="font-normal text-base font-bangla">${wordDetails.meaning}</p>
+                    </div>
+                    <div>
+                        <p class="font-semibold text-lg">Example</p>
+                        <p class="font-normal text-base text-gray-700">${wordDetails.sentence}</p>
+                    </div>
+                    <div>
+                        <p class="font-semibold text-lg font-bangla">সমার্থক শব্দ গুলো</p>
+                        <div class="synonym-btns flex gap-3">
+                            <button class="btn">Enthusiastic</button>
+                            <button class="btn">Enthusiastic</button>
+                            <button class="btn">Enthusiastic</button>
+                        </div>
+                    </div>
+                </div>
+        `
+
+        word_details.showModal()
+    }
+
+    // Blank lesson error message
 const getWords = (wordArray) => {
     // Get the Words Container
     const getWordContainer = document.getElementById('words-container-id');
@@ -37,8 +78,9 @@ const getWords = (wordArray) => {
             <h1 class="font-bangla text-3xl font-bold">নেক্সট Lesson এ যান</h1>
         </div>
         `;
+        return
     }
-    
+
     wordArray.forEach(wordObj => {
         const word = wordObj.word;
         const pronounce = wordObj.pronunciation;
@@ -52,16 +94,12 @@ const getWords = (wordArray) => {
                     <p>Meaning / Pronounciation</p>
                     <h1 class="text-2xl font-semibold text-gray-700 font-bangla">${meaning ? meaning : 'অর্থ পাওয়া যায়নি'} / ${pronounce ? pronounce : 'উচ্চারণ পাওয়া যায়নি'}</h1>
                     <div class="word-card-btn flex justify-between items-center mt-7">
-                        <button class="w-12 h-12 rounded-sm bg-blue-100 text-gray-700 hover:bg-blue-300 cursor-pointer"><i class="fa-solid fa-circle-info"></i></button>
+                        <button onclick="levelWordDetails(${wordObj.id})" class="w-12 h-12 rounded-sm bg-blue-100 text-gray-700 hover:bg-blue-300 cursor-pointer"><i class="fa-solid fa-circle-info"></i></button>
                         <button class="w-12 h-12 rounded-sm bg-blue-100 text-gray-700 hover:bg-blue-300 cursor-pointer"><i class="fa-solid fa-volume-high"></i></button>
                     </div>
                 </div>
             `
-
-        getWordContainer.appendChild(createNewElement)
-        
-
-        
+        getWordContainer.appendChild(createNewElement)  
     })
 }
 
